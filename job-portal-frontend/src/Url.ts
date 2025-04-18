3a52d8b6487f03a30a9836970830de6cd2b05710
Mahesh18 <kmahesh18001@gmail.com>
2025-04-18 21:04:50 +0530

    fixed bugs

--- job-portal-frontend/src/Url.ts
+++ job-portal-frontend/src/Url.ts
@@ -1,3 +1,3 @@
-export const baseUrl =  `http://localhost:3000`;
+// export const baseUrl =  `http://localhost:3000`;
 
-// `https://job-portal-5bc5.onrender.com` 
+export const baseUrl = `https://job-portal-5bc5.onrender.com` ;
